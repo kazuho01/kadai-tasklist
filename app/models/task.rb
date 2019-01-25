a667c9ba4785c23ee76e5fe54938c37a30b2e74b
kazuho <kazuho.git@gmail.com>
2019-01-25 10:30:54 +0000

    bootstrap

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 255, message: "255文字以下で記入してください" }
+  validates :status, presence: true, length: { maximum: 10, message: "10文字以下で記入してください" }
 end
